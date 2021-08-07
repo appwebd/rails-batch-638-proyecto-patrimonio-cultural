0e7dbe9fe15612b3d37cdfae4c85c7e57150cd55
Patricio Rojas Ortiz <patricio-rojaso@outlook.com>
2021-08-06 21:02:48 -0400

    Se corrige error de migración provincias
    Se elimina columna t.integer region_id dado que está ya definida como clave foranea.

--- src/db/migrate/20210807004427_create_provincia.rb
+++ src/db/migrate/20210807004427_create_provincia.rb
@@ -2,7 +2,6 @@ class CreateProvincia < ActiveRecord::Migration[6.1]
   def change
     create_table :provincia do |t|
       t.string :nombre
-      t.integer :region_id
 
       t.references :region, null:false, foreign_key: true
       t.timestamps
