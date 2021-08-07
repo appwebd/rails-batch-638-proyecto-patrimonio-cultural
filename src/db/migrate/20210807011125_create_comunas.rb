35e3b057ffb9b181d2a5d052563021ea48f8284e
Patricio Rojas Ortiz <patricio-rojaso@outlook.com>
2021-08-06 21:22:03 -0400

    rails generate model Comuna

---
+++ src/db/migrate/20210807011125_create_comunas.rb
@@ -0,0 +1,10 @@
+class CreateComunas < ActiveRecord::Migration[6.1]
+  def change
+    create_table :comunas do |t|
+      t.string :nombre
+
+      t.references :provincia, null:false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
