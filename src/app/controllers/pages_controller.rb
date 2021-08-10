1980ec92903001586a182b019776ccbbdebd8201
Patricio Rojas Ortiz <patricio-rojaso@outlook.com>
2021-08-10 11:51:32 -0400

    Se general algunas vistas basadas en diseño gráfico

--- src/app/controllers/pages_controller.rb
+++ src/app/controllers/pages_controller.rb
@@ -1,6 +1,19 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [ :contacto, :home, :politica_de_privacidad, :termino_de_uso  ]
+
+  def contacto
+    render ('contacto')
+  end
 
   def home
   end
+
+  def politica_de_privacidad
+    render ('politica-de-privacidad')
+  end
+
+  def termino_de_uso
+    render ('terminos-de-uso')
+  end
+
 end
