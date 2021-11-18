02dca479205d17fcb6d6b86f2df53c094561aceb
Patricio Rojas Ortiz <patricio-rojaso@outlook.com>
2021-11-18 10:21:11 -0300

    was removed empty lines

--- src/app/javascript/packs/application.js
+++ src/app/javascript/packs/application.js
@@ -3,7 +3,6 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
@@ -30,4 +29,3 @@ document.addEventListener('turbolinks:load', () => {
     initMapbox();
     initAutocomplete();
 });
-
