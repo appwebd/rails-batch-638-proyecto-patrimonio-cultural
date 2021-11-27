71ed121ba4cdd87c790763a3994f3c7e740af1b0
Patricio Rojas Ortiz <patricio-rojaso@outlook.com>
2021-11-27 20:02:59 -0300

    Updated from heroku sources.

--- src/app/helpers/user_helper.rb
+++ src/app/helpers/user_helper.rb
@@ -1,17 +1,2 @@
 module UserHelper
-  def user_show_avatar(class_avatar = 'avatar')
-    if @user.photo.key.nil?
-      image_tag "avatar.png", class: "#{class_avatar} dropdown-toggle",
-                              id: "navbarDropdown", data: { toggle: "dropdown" },
-                              'aria-haspopup': true, 'aria-expanded': false
-    else
-      cl_image_tag @user.photo.key, class: "avatar dropdown-toggle",
-                                    id: "navbarDropdown", data: { toggle: "dropdown" },
-                                    'aria-haspopup': true, 'aria-expanded': false
-    end
-  rescue StandardError
-    image_tag "avatar.png", class: "#{class_avatar} dropdown-toggle",
-                            id: "navbarDropdown", data: { toggle: "dropdown" },
-                            'aria-haspopup': true, 'aria-expanded': false
-  end
 end
