6299a939a67be77b0fc3c9fb29d6d41b51af2421
Patricio Rojas Ortiz <patricio-rojaso@outlook.com>
2021-11-27 20:00:06 -0300

    Updated routes from heroku sources.

--- src/config/routes.rb
+++ src/config/routes.rb
@@ -1,11 +1,6 @@
 # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
 
-  #get 'errors/not_found'
-  #get 'errors/internal_server_error'
-
-
-
   devise_for :users,
              controllers: {
                sessions: "users/sessions",
@@ -14,17 +9,21 @@ Rails.application.routes.draw do
              }
 
   root to: 'pages#home'
+
   post '/resultado', to: 'pages#resultado', as: :resultado
   get '/resultado', to: 'pages#home'
   get '/terminos-de-uso', to: 'pages#termino_de_uso'
   get '/politica-de-privacidad', to: 'pages#politica_de_privacidad'
 
   resources :patrimonios
+  resources :calificaciones #, only: [:new, :index, :create, :update]
 
   # Editar el perfil de usuario
   authenticate :user do
     resources :users, only: [:index, :edit, :update, :show, :destroy]
   end
+
+
 
   match "/404", to: "errors#not_found", via: :all
   match "/500", to: "errors#internal_server_error", via: :all
