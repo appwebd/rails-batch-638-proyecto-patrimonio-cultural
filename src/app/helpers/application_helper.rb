90596291c2d6e0c2234f93708fb723aeafa08c01
Patricio Rojas Ortiz <patricio-rojaso@outlook.com>
2021-11-27 19:55:59 -0300

    helpers updated from heroku sources

--- src/app/helpers/application_helper.rb
+++ src/app/helpers/application_helper.rb
@@ -1,4 +1,5 @@
 module ApplicationHelper
+  MONTHS = %w[enero febrero marzo abril mayo junio julio agosto septiembre octubre noviembre diciembre]
   def container_begin
     raw "<br/><div class='container'>"
   end
@@ -60,7 +61,54 @@ module ApplicationHelper
       cl_image_tag registro.photo.key, class: "imagen_patrimonio"
     end
 
-    rescue StandardError
-      image_tag "default.jpg", class: "imagen_patrimonio"
+  rescue StandardError
+    image_tag "default.jpg", class: "imagen_patrimonio"
+  end
+
+  def show_avatar(user_id, class_avatar = ' avatar ')
+    user = User.find(user_id)
+    if user.photo.key.nil?
+      raw '<i class="fas fa-user fa-3x avatar-calificaciones"></i>'
+    else
+      cl_image_tag user.photo.key, class: " avatar "
+    end
+  end
+
+  def user_show_avatar(class_avatar = 'avatar')
+    if @user.photo.key.nil?
+      image_tag "avatar.png", class: "#{class_avatar} dropdown-toggle",
+                id: "navbarDropdown", data: { toggle: "dropdown" },
+                'aria-haspopup': true, 'aria-expanded': false
+    else
+      cl_image_tag @user.photo.key, class: "avatar dropdown-toggle",
+                   id: "navbarDropdown", data: { toggle: "dropdown" },
+                   'aria-haspopup': true, 'aria-expanded': false
+    end
+  rescue StandardError
+    image_tag "avatar.png", class: "#{class_avatar} dropdown-toggle",
+              id: "navbarDropdown", data: { toggle: "dropdown" },
+              'aria-haspopup': true, 'aria-expanded': false
+  end
+
+  def ranking_star_show(ranking)
+    result = ''
+    ranking.times do
+      result = "#{result}<em class=\"fas fa-star five-star-color\"></em>"
+    end
+    ranking = 5 - ranking
+    ranking.times do
+      result = "#{result}<em class=\"far fa-star five-star-color\"></em>"
+    end
+    raw result
+  end
+
+
+  def date_to_words(date)
+    date_string =   date.to_s
+    year = date_string[0, 4]
+    month = date_string[5, 2].to_i
+    day = date_string[8, 2]
+    month = MONTHS[month]
+    "#{day} #{month} #{year}"
   end
 end
